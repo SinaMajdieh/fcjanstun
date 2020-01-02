bf30780c433f1a398fd21076b7fb55feddc5ba84
Sina <sina.majdieh@gmail.com>
2020-01-02 05:00:05 -0500

    Declare Flags , Goals , Lines and Ball maps and structs in world model

--- team/team.go
+++ team/team.go
@@ -1,6 +1,8 @@
 package team
 
 import (
+	"fmt"
+
 	"github.com/pattack/rcss"
 )
 
@@ -8,6 +10,42 @@ type janstun struct {
 	name  string
 	side  rcss.Side
 	match rcss.Match
+	world World
+}
+
+type World struct {
+	// LeftLine struct {
+	// 	Dis  float64
+	// 	Dir  float64
+	// 	Time int
+	// }
+	// RightLine struct {
+	// 	Dis  float64
+	// 	Dir  float64
+	// 	Time int
+	// }
+	// TopLine struct {
+	// 	Dis  float64
+	// 	Dir  float64
+	// 	Time int
+	// }
+	// BottomLine struct {
+	// 	Dis  float64
+	// 	Dir  float64
+	// 	Time int
+	// }
+
+	Ball struct {
+		Dis     float64
+		Dir     float64
+		DisChng float64
+		DirChng float64
+		Time    float64
+	}
+
+	Flags map[string]rcss.Flag
+	Goals map[string]rcss.Goal
+	Lines map[string]rcss.Line
 }
 
 func (j janstun) Name() string {
@@ -18,7 +56,9 @@ func (j *janstun) SetSide(side rcss.Side) {
 }
 func (j *janstun) Invite(m rcss.Match, unum rcss.UniformNumber) {
 	j.match = m
+	fmt.Println("Joined")
 	j.match.Move(0, 0)
+
 }
 func (j *janstun) SetPlayMode(mode rcss.PlayMode) {
 
@@ -37,10 +77,6 @@ func (j *janstun) PlayerType(pt rcss.PlayerType) {
 	//fmt.Printf("Player Type: %#v\n", pt)
 }
 
-func (j *janstun) See() {
-
-}
-
 func (j *janstun) Hear() {
 
 }
@@ -54,11 +90,21 @@ func (j *janstun) Score() {
 }
 
 func (j *janstun) Kickoff() {
+	j.match.Kick(100, 0)
+	//j.match.See()
+	//fmt.Println(j.world.Flags[Center].Distance())
+	// for j.world.Flags[Center].Distance() != 0 {
+	// 	//fmt.Println("", j.world.Flags[Center].Distance(), j.world.Flags[Center].DataArriveTime())
+	// 	j.match.Dash(100)
+	// }
 
 }
 
 func NewTeam(name string) rcss.Team {
-	return &janstun{
-		name: name,
-	}
+	var j janstun
+	j.name = name
+	j.world.Lines = make(map[string]rcss.Line)
+	j.world.Goals = make(map[string]rcss.Goal)
+	j.world.Flags = make(map[string]rcss.Flag)
+	return &j
 }
